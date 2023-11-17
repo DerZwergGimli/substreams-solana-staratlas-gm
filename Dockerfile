1a92a362cc8e48914c0802b784efab38d2a56dbe
Skullnbones <skullandbonesdao@gmail.com>
2023-11-18 00:14:52 +0100

    updating substreamkey

--- Dockerfile
+++ Dockerfile
@@ -44,8 +44,7 @@ COPY /substreams.wasm /
 RUN ls /
 
 COPY /env_init.sh /
-RUN chmod +x ./env_init.sh
-RUN ./env_init.sh
+RUN source ./env_init
 
 
 
--- env_init.sh
+++
@@ -1,1 +0,0 @@
-export SUBSTREAMS_API_TOKEN=$(curl https://auth.streamingfast.io/v1/auth/issue -s --data-binary '{"api_key":"'$STREAMINGFAST_KEY'"}' | jq -r .token)
