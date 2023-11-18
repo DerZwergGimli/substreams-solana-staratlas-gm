01158f67f252f1e8d56c6bc8a41b0c3cb08d31dd
Skullnbones <skullandbonesdao@gmail.com>
2023-11-18 18:07:37 +0100

    updating substreamkey

--- scripts/run_substream.sh
+++ scripts/run_substream.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 source env_init
-substreams-sink-sql run $DB substreams.yaml -e mainnet.sol.streamingfast.io:443 142384016: --irreversible-only
+substreams-sink-sql run $DB substreams.yaml -e mainnet.sol.streamingfast.io:443 142384016: --final-blocks-only
