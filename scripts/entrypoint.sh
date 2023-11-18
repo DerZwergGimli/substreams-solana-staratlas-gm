322c1f48f9141c5a75bd5312b3a3b4a9b9638801
Skullnbones <skullandbonesdao@gmail.com>
2023-11-18 19:03:16 +0100

    updating substreamkey

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-SUBSTREAMS_API_TOKEN=$(curl https://auth.streamingfast.io/v1/auth/issue -s --data-binary "{"api_key":"$STREAMINGFAST_KEY"}" | jq -r .token)
-export SUBSTREAMS_API_TOKEN
+export SUBSTREAMS_API_TOKEN=$(curl https://auth.streamingfast.io/v1/auth/issue -s --data-binary '{"api_key":"'$STREAMINGFAST_KEY'"}' | jq -r .token)
 
 echo $SUBSTREAMS_API_TOKEN
 echo "Starting..."
