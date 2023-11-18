be394d1a5b4d521e0a592eea59471389a0a93b14
Skullnbones <skullandbonesdao@gmail.com>
2023-11-18 18:36:40 +0100

    updating substreamkey

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,6 @@
 
 SUBSTREAMS_API_TOKEN=$(curl https://auth.streamingfast.io/v1/auth/issue -s --data-binary "{"api_key":"$STREAMINGFAST_KEY"}" | jq -r .token)
 export SUBSTREAMS_API_TOKEN
-substreams-sink-sql run psql://dev-node:insecure-change-me-in-prod@postgres:5432/dev-node?sslmode=disable substreams.yaml -e mainnet.sol.streamingfast.io:443 142384016: --final-blocks-only
+substreams-sink-sql run "psql://dev-node:insecure-change-me-in-prod@postgres:5432/dev-node?sslmode=disable" substreams.yaml -e mainnet.sol.streamingfast.io:443 142384016: --final-blocks-only
 
 exec "$@"
