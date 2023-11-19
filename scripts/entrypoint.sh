ddebc74960d28937bf3543a8309eeabf45ae6b05
Skullnbones <skullandbonesdao@gmail.com>
2023-11-19 20:27:11 +0100

    changed to final --blocks-only

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,6 +5,6 @@ export SUBSTREAMS_API_TOKEN=$(curl https://auth.streamingfast.io/v1/auth/issue -
 echo $SUBSTREAMS_API_TOKEN
 echo "Starting..."
 
-substreams-sink-sql $SINK_MODE $SINK_DB substreams.yaml -e mainnet.sol.streamingfast.io:443 142384016: --irreversible-only
+substreams-sink-sql $SINK_MODE $SINK_DB substreams.yaml -e mainnet.sol.streamingfast.io:443 142384016: --final-blocks-only
 
 exec "$@"
